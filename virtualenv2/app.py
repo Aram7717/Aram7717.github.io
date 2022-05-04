96da13d3ef0e18792787488e3099c2604956d4a5
Aram7717 <77224893+Aram7717@users.noreply.github.com>
2022-05-04 01:44:12 -0700

    Update app.py
    update email address

--- virtualenv2/app.py
+++ virtualenv2/app.py
@@ -10,8 +10,8 @@ app.secret_key = 'JigsawWeb1123577'
 
 app.config['MAIL_SERVER'] = "smtp.gmail.com"
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = '#'
-app.config['MAIL_PASSWORD'] = 'jigsaw1123577'
+app.config['MAIL_USERNAME'] = 'aram.moh@yandex.com'
+app.config['MAIL_PASSWORD'] = 'TAwiq3RUqEVaF2X'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
@@ -34,7 +34,7 @@ def contact():
             flash('All fields are required.')
             return render_template('contact.html', form=form, date=date)
         else:
-            msg = Message(form.subject.data, sender='#',
+            msg = Message(form.subject.data, sender='aram.moh@yandex.com',
                           recipients=['mraram77@gmail.com'])
 
             msg.body = """
